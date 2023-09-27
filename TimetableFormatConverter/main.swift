8cc4b7ac72b38bced515955f45b6d0d7e0d34f91
David Cormell <d.cormell@etoncollege.org.uk>
2023-09-27 15:34:24 +0100

    Adding testing xlsx file and starter code for CoreXLSX

--- TimetableFormatConverter/main.swift
+++ TimetableFormatConverter/main.swift
@@ -6,6 +6,29 @@
 //
 
 import Foundation
+import CoreXLSX
 
-print("Hello, World!")
+if let path = Bundle.main.path(forResource: "JMBFormatTrial", ofType: "xlsx") {
+    guard let xlsx = XLSXFile(filepath: path) else {
+        fatalError("XLSX file at \(path) is corrupted or does not exist")
+    }
+    
+    for wbk in try xlsx.parseWorkbooks() {
+      for (name, path) in try xlsx.parseWorksheetPathsAndNames(workbook: wbk) {
+        if let worksheetName = name {
+          print("This worksheet has a name: \(worksheetName)")
+        }
+
+        let worksheet = try xlsx.parseWorksheet(at: path)
+        for row in worksheet.data?.rows ?? [] {
+          for c in row.cells {
+            print(c)
+          }
+        }
+      }
+    }
+} else {
+    print("Unable to find excel timetable file in main bundle")
+}
+
 
